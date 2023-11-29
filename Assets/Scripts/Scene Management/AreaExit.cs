ef2997352a8c2d210f863c866c2a634af5ce5510
Genevieve Schultz <genschultz@gmail.com>
2023-11-28 16:03:10 -0800

    portals, lighting effects

--- Assets/Scripts/Scene Management/AreaExit.cs
+++ Assets/Scripts/Scene Management/AreaExit.cs
@@ -6,12 +6,21 @@ using UnityEngine.SceneManagement;
 public class AreaExit : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
+    [SerializeField] private string sceneTransitionName;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
-            SceneManager.LoadScene(sceneToLoad);
+            SceneManagement.Instance.SetTransitionName(sceneTransitionName);
+            UIFade.Instance.FadeToBlack();
+            StartCoroutine(LoadSceneRoutine());
         }
+    }
+
+    private IEnumerator LoadSceneRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
