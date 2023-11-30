0d391e8bfc718f3cd821d4cace0d3f4ac2576683
Genevieve Schultz <genschultz@gmail.com>
2023-11-29 19:29:50 -0800

    projectile improvements, destructible or not items

--- Assets/Scripts/Misc/Destructible.cs
+++ Assets/Scripts/Misc/Destructible.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destructable : MonoBehaviour
+public class Destructible : MonoBehaviour
 {
     [SerializeField] private GameObject destroyVFX;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.GetComponent<DamageSource>())
+        if (other.gameObject.GetComponent<DamageSource>() || other.gameObject.GetComponent<Projectile>())
         {
             Instantiate(destroyVFX, transform.position, Quaternion.identity);
             Destroy(gameObject);
