6d0abbcb5d82a380db7954368ea15a45f275bc40
Genevieve Schultz <genschultz@gmail.com>
2023-12-05 15:43:04 -0800

    added coin pickups

--- Assets/Scripts/Misc/Destructible.cs
+++ Assets/Scripts/Misc/Destructible.cs
@@ -9,6 +9,7 @@ public class Destructible : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.GetComponent<DamageSource>() || other.gameObject.GetComponent<Projectile>())
         {
+            GetComponent<PickupSpawner>().DropItems();
             Instantiate(destroyVFX, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
---
+++ Assets/Scripts/Misc/Pickups.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    [SerializeField] private float pickupDistance = 5f;
+    [SerializeField] private float accelerationRate = .2f;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 1.5f;
+    [SerializeField] private float popDuration = 1f;
+
+    private Vector3 moveDir;
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start() {
+        Vector3 endPos = transform.position + new Vector3(Random.Range(-0.5f, 0.5f), 0);
+
+        StartCoroutine(AnimCurveSpawnRoutine(transform.position, endPos));
+    }
+
+    private void Update() {
+        Vector3 playerPos = PlayerController.Instance.transform.position;
+
+        if (Vector3.Distance(transform.position, playerPos) < pickupDistance)
+        {
+            moveDir = (playerPos - transform.position).normalized;
+            moveSpeed += accelerationRate;
+        } else
+        {
+            moveDir = Vector3.zero;
+            moveSpeed = 0;
+        }
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<PlayerController>())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator AnimCurveSpawnRoutine(Vector3 startPosition, Vector3 endPosition)
+    {
+        float timePassed = 0f;
+
+        while (timePassed < popDuration)
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / popDuration;
+            float heightT = animCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(startPosition, endPosition, linearT) + new Vector2(0f, height);
+
+            yield return null;
+        }
+    }
+}
